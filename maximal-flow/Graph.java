24b58891f9fea67cc80d7e9204517443eb8fe3e5
David <108718458+davtwd@users.noreply.github.com>
2023-05-08 16:48:17 +0200

    Redesign to max-flow-min-cost using Dijkstra's shortest path

--- maximal-flow/Graph.java
+++ maximal-flow/Graph.java
@@ -1,7 +1,8 @@
 import java.util.ArrayList;
+import java.util.PriorityQueue;
 
 public class Graph {
-
+	
 	private ArrayList<Vertex> vertexList;
 	private ArrayList<Edge> edgeList;
 
@@ -19,58 +20,85 @@ public class Graph {
 	public void getMaxFlow(int source, int sink) {
 
 		int maxFlow = 0;
+		int minCost = 0;
 		int[] parent = new int[vertexList.size()];
-
-		while (bfs(source, sink, parent)) {
-
+	
+		while (dijkstra(source, sink, parent)) {
+	
 			int pathFlow = Integer.MAX_VALUE;
-
+			int pathCost = 0;
+	
 			for (int v = sink; v != source; v = parent[v]) {
 				int u = parent[v];
 				for (Edge e : this.edgeList) {
 					if (e.getA() == u && e.getB() == v) {
 						pathFlow = Math.min(pathFlow, e.getCapacity() - e.getFlow());
+						pathCost += e.getCost();
 					}
 				}
 			}
-
+	
 			for (int v = sink; v != source; v = parent[v]) {
 				int u = parent[v];
+				boolean foundForwardEdge = false;
+				boolean foundReverseEdge = false;
+				Edge reverseEdge = null;
+
 				for (Edge e : this.edgeList) {
 					if (e.getA() == u && e.getB() == v) {
 						e.addFlow(pathFlow);
+						foundForwardEdge = true;
+						reverseEdge = e.getReverseEdge();
 					} else if (e.getA() == v && e.getB() == u) {
 						e.addFlow(-pathFlow);
+						foundReverseEdge = true;
+					}
+				}
+					
+				if (foundForwardEdge && !foundReverseEdge) {
+					this.edgeList.add(reverseEdge);
+				}
+			}
+
+			maxFlow += pathFlow;
+			minCost += pathFlow * pathCost;
+		}
+
+		System.out.println(String.format("Output [source, sink, max-flow, min-cost]: %d, %d, %d, %d", source, sink, maxFlow, minCost));
+	}
+
+	private boolean dijkstra(int source, int sink, int[] parent) {
+
+		int[] dist = new int[vertexList.size()];
+		
+		for (int i = 0; i < dist.length; i++) {
+			dist[i] = Integer.MAX_VALUE;
+		}
+
+		dist[source] = 0;
+
+		boolean[] visited = new boolean[vertexList.size()];
+
+		PriorityQueue<Integer> epsilon = new PriorityQueue<>((u, v) -> dist[u] - dist[v]);
+		epsilon.add(source);
+
+		while (!epsilon.isEmpty()) {
+			int root = epsilon.poll();
+			visited[root] = true;
+
+			for (Edge e : this.edgeList) {
+				if ((e.getCapacity() - e.getFlow()) > 0 && e.getA() == root) {
+					int neighbor = e.getB();
+					int alt = dist[root] + e.getCost();
+					if (alt < dist[neighbor]) {
+						dist[neighbor] = alt;
+						parent[neighbor] = root;
+						epsilon.add(neighbor);
 					}
 				}
 			}
-			maxFlow += pathFlow;
 		}
 
-		System.out.println(String.format("Output [source, sink, max-flow]: %d, %d, %d", source, sink, maxFlow));
-	}
-
-	private boolean bfs(int source, int sink, int[] parent) { // breadth-first search 
-
-		boolean[] visited = new boolean[vertexList.size()];
-		ArrayList<Integer> epsilon = new ArrayList<>();
-		epsilon.add(source);
-		visited[source] = true;
-		parent[source] = -1;
-	
-		while (!epsilon.isEmpty()) {
-			int root = epsilon.remove(0);
-	
-			for (Edge e : this.edgeList) {
-				if (!visited[e.getB()] && (e.getCapacity() - e.getFlow()) > 0 && e.getA() == root) {
-					visited[e.getB()] = true;
-					parent[e.getB()] = root;
-					epsilon.add(e.getB());
-				}
-			}
-
-		}
-	
 		return visited[sink];
 	}
 }
